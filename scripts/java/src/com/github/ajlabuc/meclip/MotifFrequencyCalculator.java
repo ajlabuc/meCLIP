0263c97dea302da8916f10aae7ef57cebb6a199c
Justin <Justin@192.168.1.12>
2020-12-14 10:15:13 -0700

    - Extract (not package) BioJava libraries into meCLIP.jar

--- scripts/java/src/com/github/ajlabuc/meclip/MotifFrequencyCalculator.java
+++ scripts/java/src/com/github/ajlabuc/meclip/MotifFrequencyCalculator.java
@@ -10,7 +10,7 @@ import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.biojava.nbio.core.sequence.*;
+import org.biojava.nbio.core.sequence.DNASequence;
 import org.biojava.nbio.core.sequence.io.FastaReaderHelper;
 
 /**
